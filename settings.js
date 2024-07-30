51f5c74bd323e14ee94fe30387aa2a77fd79b6d7
theclaay <mrzooombiex@gmail.com>
2024-07-30 11:01:58 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923278067269"
+global.ownername = "🐛 MR AyaZKing "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
+global.ownernumber = '923278067269'  //creator number
+global.ownername = ' MR Ayazking ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
 //sticker details
